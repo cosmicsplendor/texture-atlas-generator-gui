ec97fd163d304695f2682612b757ce29231c42a7
xtropia <ufocalypse@gmail.com>
2021-01-20 17:42:44 +0545

    Implement strip packing function

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
-import React, { useState, useEffect } from "react"
+import React from "react"
 import reactDOM from "react-dom"
 
-const  GithubUser = ({ username }) => {
-    const [ userInfo, setUserInfo ] = useState(null)
-
-    useEffect(() => {
-       fetch(`https://api.github.com/users/${username}`)
-       .then(res => res.json())
-       .then(setUserInfo)
-    }, [ username ])
-
-    return userInfo ? <pre style={{margin: "2em auto", width: "50vw"}}>{JSON.stringify(userInfo, null, 4)}</pre>: "User data hasn't loaded yet."
+const  App = () => {
+    return (
+        <h1> this app is yet to see the light of the dawn </h1>
+    )
 }
 
-reactDOM.render(<GithubUser username="xtropia"/>, document.querySelector("#app"))
+reactDOM.render(<App />, document.querySelector("#app"))
