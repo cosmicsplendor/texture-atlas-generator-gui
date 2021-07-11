eca81e4b37fe99b9ec214bf37ca94fd0abd1d98f
unknown <ufocalypse@gmail.com>
2021-07-11 15:31:48 +0545

    Fix other bugs from previous commits

--- src/utils/texAtlas/index.js
+++ src/utils/texAtlas/index.js
@@ -7,32 +7,36 @@ import { PREVIEW_ID, HBOX_EDITOR_W, HBOX_EDITOR_IMG_W, HBOX_SLIDER_W } from "../
 const HBOX_EDITOR_IMG_OFFSET = (HBOX_EDITOR_W - HBOX_EDITOR_IMG_W) / 2
 
 const spriteToTexture = sprite => {
-    const { src, name, id, hitboxSlider, anchor, anchorPoint, hitboxShape } = sprite
+    const { src, name, id, hitboxSlider, anchor, anchorPoint, hitshape } = sprite
     const tex = new Texture({ imgUrl: src })
     tex.width = tex.img.width
     tex.height = tex.img.height
     tex.name = name.replace("\..+$", "")
     const { round, max } = Math
     if (anchor) {
-        const maxDim = max(width, height)
-        tex.anchorPoint = {
-            x: round(maxDim * (tex.anchorPoint.x - HBOX_EDITOR_IMG_OFFSET) / HBOX_EDITOR_IMG_W),
-            y: round(maxDim * (tex.anchorPoint.y - HBOX_EDITOR_IMG_OFFSET) / HBOX_EDITOR_IMG_W)
-        }
+        const maxDim = max(tex.width, tex.height)
+        const xOffset = -(maxDim - tex.width) / 2
+        const yOffset = -(maxDim - tex.height) / 2
+        tex.anchorPoint = anchorPoint ? 
+        {
+            x: round(maxDim * (anchorPoint.x - HBOX_EDITOR_IMG_OFFSET) / HBOX_EDITOR_IMG_W) + xOffset,
+            y: round(maxDim * (anchorPoint.y - HBOX_EDITOR_IMG_OFFSET) / HBOX_EDITOR_IMG_W) + yOffset
+        } :
+        { x: round(tex.width / 2), y: round(tex.height / 2)}
     }
     if (!!hitboxSlider) {
         const { hor: [ x1, x2 ], vert: [ y1, y2 ] } = hitboxSlider
         const { width, height } = tex
         const maxDim = max(width, height)
-        const offsetX = - (maxDim - width) / 2
-        const offsetY = - (maxDim - height) / 2
+        const offsetX = -(maxDim - width) / 2
+        const offsetY = -(maxDim - height) / 2
         tex.hitbox = {
             x: round(maxDim * x1 / HBOX_SLIDER_W) + offsetX,
             y: round(maxDim * y1 / HBOX_SLIDER_W) + offsetY,
             width: round(maxDim * (x2 - x1)  / HBOX_SLIDER_W),
             height: round(maxDim * (y2 - y1) / HBOX_SLIDER_W)
         }
-        if (hitboxShape === "CIRCLE") {
+        if (hitshape === "CIRCLE") {
             tex.hitCirc = {
                 x: tex.hitbox.x,
                 y: tex.hitbox.y,
